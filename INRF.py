f83d640b10de42190c8c8dc4716cbaf70a6f8c80
ewrfcas <ccjdurandal422@163.com>
2017-12-17 16:22:16 +0800

    update for lr

--- INRF.py
+++ INRF.py
@@ -2,9 +2,11 @@ import numpy as np
 import time as time_sys
 from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 import DataToolBox
 
-def test(INRFStruct, test_data):
+def test_RF(INRFStruct, test_data):
     L=len(INRFStruct)
     N=len(INRFStruct[0])
     test_data_temp = test_data
@@ -24,7 +26,7 @@ def test(INRFStruct, test_data):
             predict_label[i] = 0
     return predict_label,values
 
-def fun(l,n, p, nTree, train_data, train_label):
+def fun_RF(l,n, p, nTree, train_data, train_label):
     train_data_alter = train_data
     m_acc = []
     INRFStruct=[]
@@ -63,7 +65,7 @@ def fun(l,n, p, nTree, train_data, train_label):
         inter_feature = inter_feature / n
         train_data_alter = np.hstack((train_data, inter_feature))
         # 训练集评估
-        [predict_label,_] = test(INRFStruct, train_data)
+        [predict_label,_] = test_RF(INRFStruct, train_data)
         [_,_,macc]=DataToolBox.get_macc(train_label,predict_label)
         m_acc.append(macc)
         if i >= 2 and m_acc[-1] <= m_acc[-1 - 1]:
@@ -72,14 +74,133 @@ def fun(l,n, p, nTree, train_data, train_label):
 
     return INRFStruct
 
+def test(NBCCIStruct,feature,test_data):
+    test_data_temp=test_data
+    L=len(NBCCIStruct)#层数
+    N=len(NBCCIStruct[0])#样本bagging次数
+    for i in range(0,L):
+        values = np.zeros((test_data.shape[0], 2))
+        for j in range(0,N):
+            values =values+ train_test(NBCCIStruct[i][j], feature[i][j], test_data_temp)
+        values=values/N
+        test_data_temp = np.hstack((test_data, values))
+    predict_label = np.empty([values.shape[0]])
+    for i in range(0, values.shape[0]):
+        if values[i, 1] > values[i, 0]:
+            predict_label[i] = 1
+        else:
+            predict_label[i] = 0
+    return predict_label, values
 
-def main(data_name, file_name, train_data_all, test_data_all, params):
+#训练内部插值
+def train_test(NBStruct,feature_temp,data):
+    F=len(NBStruct)
+    confidence=np.zeros((data.shape[0],2))
+    for i in range(0,F):
+        confidence=confidence+NBStruct[i].predict_proba(data[:,feature_temp[i]])
+    confidence=confidence/F
+
+    return confidence
+
+def NB_FBagging(data,label,f,rf):
+    fp=int(data.shape[1]*rf)
+    NBStruct=[]
+    feature=[]
+    for i in range(0,f):
+        #特征随机
+        index = np.arange(data.shape[1])
+        np.random.shuffle(index)
+        data_temp=data[:,index[0:fp]]
+        feature.append(index[0:fp])
+        clf = GaussianNB()
+        clf.fit(data_temp,np.ravel(label))
+        NBStruct.append(clf)
+
+    return feature,NBStruct
+
+def LR_FBagging(data,label,f,rf):
+    fp = int(data.shape[1] * rf)
+    LRStruct = []
+    feature = []
+    for i in range(0, f):
+        # 特征随机
+        index = np.arange(data.shape[1])
+        np.random.shuffle(index)
+        data_temp = data[:, index[0:fp]]
+        feature.append(index[0:fp])
+        clf = LogisticRegression(C=10000)
+        clf.fit(data_temp, np.ravel(label))
+        LRStruct.append(clf)
+
+    return feature, LRStruct
+
+def fun(l,f,n,rf,rp,train_data, train_label,base_classifier):
+    train_data_alter = train_data
+    m_acc = []
+    NBCCIStruct=[]
+    feature=[]
+    L=l
+    for i in range(0,L):
+        if i!=L-1:
+            # 插值特征(置信度)
+            inter_feature = np.zeros((len(train_label),2))
+            # 内部交叉验证 3cv
+            skf = StratifiedKFold(shuffle=True,n_splits=3)
+            for train_index, test_index in skf.split(X=train_data_alter, y=train_label):
+                data_train_temp = train_data_alter[train_index,:]
+                label_train_temp = train_label[train_index]
+                data_test_temp = train_data_alter[test_index,:]
+                # 区分多数类少数类，返回[多数类，少数类，多数类类标，少数类类标]
+                [data_much, data_less, much_label, less_label] = DataToolBox.divide_data(data_train_temp, label_train_temp)
+                for j in range(0,n):
+                    # 随机下采样
+                    [tData, tLabel] = DataToolBox.RUS(data_much, data_less, much_label, less_label)
+                    if base_classifier=='NB':
+                        feature_temp, NBStruct_temp = NB_FBagging(tData, tLabel, f, rf)  # 特征bagging NB
+                    elif base_classifier=='LR':
+                        feature_temp, NBStruct_temp = LR_FBagging(tData, tLabel, f, rf)  # 特征bagging LR
+                    inter_feature_temp = train_test(NBStruct_temp,feature_temp,data_test_temp)
+                    inter_feature_temp[:, int(much_label)] = inter_feature_temp[:, int(much_label)] * rp
+                    inter_feature_temp[:, int(less_label)] = inter_feature_temp[:, int(less_label)] / rp
+                    inter_feature[test_index,:]=inter_feature[test_index,:]+inter_feature_temp
+        # 区分多数类少数类，返回[多数类，少数类，多数类类标，少数类类标]
+        [data_much, data_less, much_label, less_label] = DataToolBox.divide_data(train_data_alter, train_label)
+        NBCCIStruct_oneLine = []
+        feature_oneLine=[]
+        for j in range(0,n):
+            # 随机下采样
+            [tData, tLabel] = DataToolBox.RUS(data_much, data_less, much_label, less_label)
+            if base_classifier=='NB':
+                feature_temp, NBStruct_temp = NB_FBagging(tData, tLabel, f, rf)  # 特征bagging NB
+            elif base_classifier=='LR':
+                feature_temp, NBStruct_temp = LR_FBagging(tData, tLabel, f, rf)  # 特征bagging NB
+            NBCCIStruct_oneLine.append(NBStruct_temp)
+            feature_oneLine.append(feature_temp)
+        NBCCIStruct.append(NBCCIStruct_oneLine)
+        feature.append(feature_oneLine)
+        # 插值
+        if L!=1:
+            inter_feature = inter_feature / n
+            train_data_alter = np.hstack((train_data, inter_feature))
+        # 训练集评估
+        [predict_label,_] = test(NBCCIStruct,feature,train_data)
+        [_,_,macc]=DataToolBox.get_macc(train_label,predict_label)
+        m_acc.append(macc)
+        if i >= 2 and m_acc[-1] <= m_acc[-1 - 1]:
+            NBCCIStruct.pop()
+            feature.pop()
+            return NBCCIStruct,feature
+
+    return NBCCIStruct,feature
+
+
+def main(data_name, file_name, train_data_all, test_data_all, params, base_classifier='RF'):
     kflod = 5
     f = open(file_name, "w")
-    best_macc=0;best_auc=0
+    best_macc=0;best_auc=0;best_macc_std=0
     for param in params:
-        f.write("Dataset-"+data_name+" L-"+str(param['L'])+" N-"+str(param['N'])+" rp-"+str(param['rp'])+"\n")
-        print("Dataset-"+data_name+" L-"+str(param['L'])+" N-"+str(param['N'])+" rp-"+str(param['rp']))
+        f.write(base_classifier+':'+data_name+' '+str(params)+"\n")
+        print(base_classifier+':'+data_name+' '+str(params))
         f.write("--------------------------------------\n") # 分隔符
         print("--------------------------------------")
         time = [];acc = [];tp = [];tn = [];level = [];m_acc = [];auc = []
@@ -91,12 +212,17 @@ def main(data_name, file_name, train_data_all, test_data_all, params):
             # 训练开始 计时
             start_time=time_sys.time()
             # 集成
-            nTree = 50
-            INRFStruct = fun(param['L'],param['N'],param['rp'], nTree, train_data, train_label)
-            time.append(time_sys.time()-start_time)
+            if base_classifier=='RF':
+                nTree = 50
+                INRFStruct = fun_RF(param['L'],param['N'],param['rp'], nTree, train_data, train_label)
+                # 测试
+                [predict_label, values]=test_RF(INRFStruct,test_data)
+            else:
+                INRFStruct, feature = fun(param['L'],param['F'], param['N'], param['rf'], param['rp'], train_data, train_label, base_classifier)
+                # 测试
+                [predict_label, values] = test(INRFStruct, feature, test_data)
+            time.append(time_sys.time() - start_time)
             level.append(len(INRFStruct))
-            # 测试
-            [predict_label, values]=test(INRFStruct,test_data)
             scores = values[:,1]
             acc.append(DataToolBox.get_acc(test_label,predict_label))
             auc.append(DataToolBox.get_auc(test_label,scores))
@@ -123,6 +249,7 @@ def main(data_name, file_name, train_data_all, test_data_all, params):
         if np.mean(m_acc)>best_macc:
             best_macc=np.mean(m_acc)
             best_macc_param=param
+            best_macc_std=np.std(m_acc)
         if np.mean(auc)>best_auc:
             best_auc=np.mean(auc)
             best_auc_param=param
@@ -131,3 +258,4 @@ def main(data_name, file_name, train_data_all, test_data_all, params):
     f.write("The best auc is: " + str(round(best_auc,4)) + ", the best param is: " + str(best_auc_param) + "\n")
     print("The best auc is: " + str(round(best_auc,4)) + ", the best param is: " + str(best_auc_param))
     f.close()
+    return best_macc,best_macc_std
--- Start.py
+++ Start.py
@@ -1,5 +1,6 @@
 import scipy.io as sio
 import INRF
+import pandas as pd
 from sklearn.model_selection import ParameterGrid
 
 data_dict=sio.loadmat('data/Imbalanced_data.mat')
@@ -7,17 +8,41 @@ data_all=data_dict['Imbalanced_data']
 name_list=data_all[:,0]
 IR_ratio=data_all[:,1]
 data_set=data_all[:,2]
-params = {
-    'L': [5], # upper bound of level(auto)
-    'N': [5], # bagging iterations
-    'rp': [0.85,0.9,0.95,1,1.05,1.1,1.15], #disturbance eta（1不变,<1偏向多数类，>1偏向少数类）
-}
+base_classifier='NB'#base classifiers which can be used in our method. selected from 'RF','LR','NB'
+if base_classifier=='NB' or 'LR':
+    params={
+        'L': [5],  # upper bound of level(auto)
+        'N': [5],  # sample bagging iterations
+        'rp': [0.85, 0.9, 0.95, 1, 1.05, 1.1, 1.15],  # disturbance eta（1不变,<1偏向多数类，>1偏向少数类)
+        'F': [5], # feature bagging iterations (only used in Naive Bayes (NB) and Logistic Regression (LR) which work as base classifiers)
+        'rf': [0.5, 0.7, 0.9],  # feature bagging rate (only used with NB and LR)
+    }
+else:
+    params = {
+        'L': [5],  # upper bound of level(auto)
+        'N': [5],  # sample bagging iterations
+        'rp': [0.85, 0.9, 0.95, 1, 1.05, 1.1, 1.15],  # disturbance eta（1不变,<1偏向多数类，>1偏向少数类)
+    }
 params = list(ParameterGrid(params))
-for name_index in range(3,len(name_list)):
+datasets=['ecoli_0_vs_1','glass1','wisconsin','pima','glass0','vehicle3','ecoli1','new_thyroid2','yeast3','ecoli3','page_blocks0','yeast-0-2-5-6_vs_3-7-8-9',
+         'ecoli-0-3-4-7_vs_5-6','glass-0-4_vs_5','yeast_0_5_6_7_9_vs_4','glass-0-6_vs_5','cleveland-0_vs_4','ecoli-0-1-4-6_vs_5','yeast_1_vs_7','ecoli4',
+         'page_blocks_1_3_vs_4','glass_0_1_6_vs_5','yeast_2_vs_8','glass5','shuttle_c2_vs_c4','yeast4','yeast_1_2_8_9_vs_7','yeast5','ecoli_0_1_3_7_vs_2_6',
+         'abalone19']
+data_name_list=[]
+best_macc_list=[]
+best_macc_std_list=[]
+for name_index in range(0,len(name_list)):
     data_name = name_list[name_index][0]
+    if data_name not in datasets:continue
     data = data_set[name_index]
     train_data_all=data[:,0]
     test_data_all=data[:,1]
     # combine the file name
     file_name = 'result/'+data_name+'.txt'
-    INRF.main(data_name, file_name, train_data_all, test_data_all, params)
+    best_macc, best_macc_std=INRF.main(data_name, file_name, train_data_all, test_data_all, params, base_classifier=base_classifier)
+    data_name_list.append(data_name)
+    best_macc_list.append(best_macc)
+    best_macc_std_list.append(best_macc_std)
+result=pd.DataFrame([data_name_list,best_macc_list,best_macc_std_list],index=['data_name','best_macc','std'])
+result=result.transpose()
+result.to_csv('report/result_'+base_classifier+'.csv')
